a629fb9e97ac60d0314cd0485701011aa7ce3de6
adarshsingh58 <adarshsingh58@gmail.com>
2017-05-24 11:41:20 +0530

    HelloSpring project Committed

---
+++ HelloSpring/src/com/adarsh/www/HellpSpring.java
@@ -0,0 +1,16 @@
+package com.adarsh.www;
+
+public class HellpSpring {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
---
+++ HelloSpring/src/com/adarsh/www/Main.java
@@ -0,0 +1,14 @@
+package com.adarsh.www;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		HellpSpring hellpSpring=(HellpSpring) applicationContext.getBean("helloSpring");
+		System.out.println(hellpSpring.getMessage());
+	}
+}
